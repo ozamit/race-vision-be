51552e67b9addb5ad721df6e42505aeb1a206551
ozamit <your.email@example.comozamit7@gmail.com>
2025-05-08 17:47:47 +0200

    fix host

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ require('dotenv').config();
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://race-vision-fe.onrender.com'
+}));
 app.use(express.json());
 
 // MongoDB Connection
